79f66eae066c17e5729ecc90cb9ea35de8f0ea04
jhonataguerra <jhonataguerra@outlook.com.br>
2022-11-07 17:35:00 -0300

    Initial data models

--- base/views.py
+++ base/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 
+from .models import Room
 
+rooms = [
+    {'id':1, 'name': 'Python'},
+    {'id':2, 'name': 'Design'},
+    {'id':3, 'name': 'Frontend'},
+]
 
 def home(request):
-    return render(request, 'base/home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-        return render(request, 'base/room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request, 'base/room.html', context)
 
 

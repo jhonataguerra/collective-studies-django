472b51b9865ab20969fdd8644c37b9282302cd65
jhonataguerra <jhonataguerra@outlook.com.br>
2022-11-09 18:46:23 -0300

    Create activity feed

--- base/views.py
+++ base/views.py
@@ -61,17 +61,19 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q) 
     )
-
-    room_count = rooms.count()
-
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics':topics, 'room_count':room_count}
+    room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    
+    context = {'rooms': rooms, 'topics':topics,
+               'room_count':room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method == "POST":
         message = Message.objects.create(
@@ -85,6 +87,12 @@ def room(request, pk):
 
     context = {'room':room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
+
+
+def userProfile(request):
+    context = {}
+    return render(request, 'base/profile.html', context)
+
 
 @login_required(login_url='login')
 def createRoom(request):

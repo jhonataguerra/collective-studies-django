17b961c8ae7c038f17411636417b0f7daec9b165
jhonataguerra <jhonataguerra@outlook.com.br>
2022-11-08 23:45:32 -0300

    add search rooms

--- base/views.py
+++ base/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q) 
+    )
+
+    room_count = rooms.count()
+
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics':topics, 'room_count':room_count}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
+    
     context = {'room':room}
     return render(request, 'base/room.html', context)
 
@@ -38,5 +49,13 @@ def updateRoom(request, pk):
 
     context = {'form':form}
     return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
 
 
